8fdd0aa27810ba2d3754be677cd58a724eef3044
Sean Collins <sgc29@zips.uakron.edu>
2019-01-29 01:10:23 -0500

    Fix bugs in CollectionGUI

--- training/src/collection_gui.py
+++ training/src/collection_gui.py
@@ -3,59 +3,63 @@ from PIL import Image
 from PIL import ImageTk
 import tkFileDialog
 import cv2 as cv
+from camera import Camera
 
 
 class CollectionGUI(tk.Frame):
 
-    def __init__(self, parent, camera, database, *args, **kwargs):
-        # Forward arguments to parent widget's constructor
-        tk.Frame.__init__(self, *args, **kwargs)
+    # Refresh rate [milliseconds]
+    REFRESH_RATE = 120
 
+    def __init__(self, parent, camera, database, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.grid()
         self.parent = parent
+        self.camera = camera
         self.database = database
         self.directory = tkFileDialog.askdirectory()
-
         self.__create_widgets()
         self.__layout_widgets()
+        self.__configure_callbacks()
+        self.update_image()
 
     def __create_widgets(self):
         self.lblTitle = tk.Label(self, text='Graphical Camera Interface', font=('Helvetica', 20))
         self.lblDirectory = tk.Label(self, text='Directory:')
-        self.lblImage(self)
-
+        self.lblImage = tk.Label(self)
         self.entryDirectory = tk.Entry(self, text=self.directory)
-
         self.butCaptureFrame = tk.Button(self, text='Capture Frame')
 
     def __layout_widgets(self):
-        self.lblTitle.grid(row=0, column=0)
+        self.lblTitle.grid(row=0, column=0, columnspan=2)
         self.lblDirectory.grid(row=1, column=0)
         self.lblImage.grid(row=2, column=0, columnspan=2)
-
         self.entryDirectory.grid(row=1, column=1)
-
         self.butCaptureFrame.grid(row=3, columnspan=2, sticky='we')
 
     def __configure_callbacks(self):
-        self.parent.after(1, self.update_image)
+        pass
 
     def update_image(self):
-        if camera.good():
+        if self.camera.good():
             # Grab frame from camera
-            self.image = camera.get_frame()
+            self.image = self.camera.get_frame()
 
             # Transform image to format recognized by GUI
-            im = cv.cvtColor(self.image, cv.COLOR_BGB2RGB)
+            im = cv.cvtColor(self.image, cv.COLOR_BGR2RGB)
             im = Image.fromarray(im)
             im = ImageTk.PhotoImage(im)
 
             # Display the image
-            self.lblImage.config(image=im)
-        else:
-            self.lblImage.config(text="Camera Failure")
+            self.lblImage.configure(image=im)
+            self.lblImage.image = im
+
+        # Refresh the image according to refresh rate
+        self.parent.after(CollectionGUI.REFRESH_RATE, self.update_image)
 
 
 root = tk.Tk()
 database = None
 camera = Camera()
 gui = CollectionGUI(root, camera, database)
+root.mainloop()
